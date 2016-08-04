64df5060c55006ee3f1c40d1967df470b3962c09
João Gonçalves <jsvgoncalves@gmail.com>
2016-08-04 16:13:48 +0200

    global: remove os2 related init
    Removes bootstrap code for Openshift Online 2
    
    Signed-off-by: Joao Goncalves <jsvgoncalves@gmail.com>

--- app.py
+++ app.py
@@ -15,24 +15,6 @@ import sys
 from flask import Flask
 from sampleapp.database import db
 
-try:
-    virtenv = os.path.join(
-        os.environ.get('OPENSHIFT_PYTHON_DIR', '.'), 'virtenv')
-    python_version = "python" + \
-        str(sys.version_info[0]) + "." + str(sys.version_info[1])
-    os.environ['PYTHON_EGG_CACHE'] = os.path.join(virtenv,
-                                                  'lib',
-                                                  python_version,
-                                                  'site-packages')
-    virtualenv = os.path.join(virtenv, 'bin', 'activate_this.py')
-    if(sys.version_info[0] < 3):
-        execfile(virtualenv, dict(__file__=virtualenv))
-    else:
-        exec(open(virtualenv).read(), dict(__file__=virtualenv))
-
-except IOError:
-    pass
-
 
 def register_blueprints(app, blueprints):
     """Registers the blueprints given by loading their module
